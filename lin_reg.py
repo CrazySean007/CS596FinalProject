9918837d6ed1a5075ed267416d2634cb3f96b57e
aaronmarveller <peiyuanl@usc.edu>
2020-12-08 14:22:34 -0800

    update

---
+++ lin_reg.py
@@ -0,0 +1,176 @@
+# coding: utf-8
+
+import math
+import matplotlib
+import numpy as np
+import pandas as pd
+
+from datetime import date, datetime, time, timedelta
+from matplotlib import pyplot as plt
+from pylab import rcParams
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+# Input params #
+stk_path = "./GOOG.csv"
+test_size = 0.2  # proportion of dataset to be used as test set
+cv_size = 0.2  # proportion of dataset to be used as cross-validation set
+Nmax = 7  # for feature at day t, we use lags from t-1, t-2, ..., t-N as features
+N = 5
+# Nmax is the maximum N we are going to test
+
+
+# # Common functions
+def get_preds_lin_reg(df, target_col, N, pred_min, offset):
+    """
+    Given a dataframe, get prediction at timestep t using values from t-1, t-2, ..., t-N.
+    Inputs
+        df         : dataframe with the values you want to predict. Can be of any length.
+        target_col : name of the column you want to predict e.g. 'adj_close'
+        N          : get prediction at timestep t using values from t-1, t-2, ..., t-N
+        pred_min   : all predictions should be >= pred_min
+        offset     : for df we only do predictions for df[offset:]. e.g. offset can be size of training set
+    Outputs
+        pred_list  : the predictions for target_col. np.array of length len(df)-offset.
+    """
+    # Create linear regression object
+    regression = LinearRegression(fit_intercept=True)
+
+    pred_list = []
+
+    for i in range(offset, len(df[target_col])):
+        X_train = np.array(range(len(df[target_col][i - N:i])))  # e.g. [0 1 2 3 4]
+        y_train = np.array(df[target_col][i - N:i])  # e.g. [2944 3088 3226 3335 3436]
+        X_train = X_train.reshape(-1, 1)
+        y_train = y_train.reshape(-1, 1)
+        regression.fit(X_train, y_train)  # Train the model
+        pred = regression.predict(N)
+
+        pred_list.append(pred[0][0])  # Predict the footfall using the model
+
+    # If the values are < pred_min, set it to be pred_min
+    pred_list = np.array(pred_list)
+
+    return pred_list
+
+
+def get_mape(y_true, y_pred):
+    """
+    Compute mean absolute percentage error (MAPE)
+    """
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+# # Load data
+
+df = pd.read_csv(stk_path, sep=",")
+
+# Convert Date column to datetime
+df.loc[:, 'Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+df_date = df['Date']
+
+# Change all column headings to be lower case, and remove spacing
+df.columns = [str(x).lower().replace(' ', '_') for x in df.columns]
+
+# Get month of each sample
+df['month'] = df['date'].dt.month
+
+# Sort by datetime
+df.sort_values(by='date', inplace=True, ascending=True)
+
+df = df.drop(["date"], axis=1)
+df = df.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
+
+df = pd.concat([df_date.rename('date'), df], axis=1)
+df.head(10)
+
+# # Split into train, dev and test set
+
+# Get sizes of each of the datasets
+num_cv = int(cv_size * len(df))
+num_test = int(test_size * len(df))
+num_train = len(df) - num_cv - num_test
+print("num_train = " + str(num_train))
+print("num_cv = " + str(num_cv))
+print("num_test = " + str(num_test))
+
+# Split into train, cv, and test
+train = df[:num_train]
+cv = df[num_train:num_train + num_cv]
+train_cv = df[:num_train + num_cv]
+test = df[num_train + num_cv:]
+print("train.shape = " + str(train.shape))
+print("cv.shape = " + str(cv.shape))
+print("train_cv.shape = " + str(train_cv.shape))
+print("test.shape = " + str(test.shape))
+
+
+# # Predict using Linear Regression
+
+RMSE = []
+R2 = []
+mape = []
+
+est_list = get_preds_lin_reg(train_cv, 'adj_close', N, 0, num_train)
+
+cv['est' + '_N' + str(N)] = est_list
+RMSE.append(math.sqrt(mean_squared_error(est_list, cv['adj_close'])))
+R2.append(r2_score(cv['adj_close'], est_list))
+mape.append(get_mape(cv['adj_close'], est_list))
+
+print('RMSE = ' + str(RMSE))
+print('R2 = ' + str(R2))
+print('MAPE = ' + str(mape))
+cv.head()
+
+# Set optimum N
+N_opt = 5
+
+# # Plot predictions on dev set
+
+# Plot adjusted close over time
+rcParams['figure.figsize'] = 10, 8  # width 10, height 8
+
+ax = train.plot(x='date', y='adj_close', style='b-', grid=True)
+ax = cv.plot(x='date', y='adj_close', style='y-', grid=True, ax=ax)
+ax = test.plot(x='date', y='adj_close', style='g-', grid=True, ax=ax)
+ax = cv.plot(x='date', y='est_N5', style='m-', grid=True, ax=ax)
+ax.legend(['train', 'dev', 'test', 'predictions with N=5'])
+ax.set_xlabel("date")
+ax.set_ylabel("USD")
+
+
+
+#Final Model
+
+#
+# est_list = get_preds_lin_reg(df, 'adj_close', N_opt, 0, num_train+num_cv)
+# test['est' + '_N' + str(N_opt)] = est_list
+# print("RMSE = %0.3f" % math.sqrt(mean_squared_error(est_list, test['adj_close'])))
+# print("R2 = %0.3f" % r2_score(test['adj_close'], est_list))
+# print("MAPE = %0.3f%%" % get_mape(test['adj_close'], est_list))
+# test.head()
+#
+# # Plot adjusted close over time, only for test set
+# rcParams['figure.figsize'] = 10, 8 # width 10, height 8
+# matplotlib.rcParams.update({'font.size': 14})
+#
+# ax = test.plot(x='date', y='adj_close', style='gx-', grid=True)
+# ax = test.plot(x='date', y='est_N5', style='rx-', grid=True, ax=ax)
+# ax.legend(['test', 'predictions using linear regression'], loc='upper left')
+# ax.set_xlabel("date")
+# ax.set_ylabel("USD")
+plt.show()
+
+# Save as csv
+test_lin_reg = cv
+
+test_lin_reg.to_csv("./test_lin_reg.csv")
+
+# # Findings
+# * On the dev set, the lowest RMSE is 1.2 which is achieved using N=1, ie. using value on day t-1 to predict value on day t
+# * On the dev set, the next lowest RMSE is 1.36 which is achieved using N=5, ie. using values from days t-5 to t-1 to predict value on day t
+# * We will use N_opt=5 in this work since our aim here is to use linear regression
+# * On the test set, the RMSE is 1.42 and MAPE is 0.707% using N_opt=5
--- my_project.py
+++ my_project.py
@@ -4,96 +4,91 @@ import tensorflow as tf
 import time
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
-from keras.layers.normalization import BatchNormalization
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import matplotlib.pyplot as plt
 
-from sklearn.preprocessing import MinMaxScaler
+# Input params #
+stk_path = "./GOOG.csv"
+cv_size = 0.2  # proportion of dataset to be used as cross-validation set
+test_size = 0.2  # proportion of dataset to be used as test set
+
 
 def readTrain():
-  train = pd.read_csv("data.txt", engine='python')
-  train.tail()
-  # print (train)
-  return train
+    train = pd.read_csv(stk_path, engine='python')
+    train.tail()
+    return train
 
 
 def augFeatures(train):
-  train["Date"] = pd.to_datetime(train["Date"])
-  train["year"] = train["Date"].dt.year
-  train["month"] = train["Date"].dt.month
-  train["date"] = train["Date"].dt.day
-  train["day"] = train["Date"].dt.dayofweek
-  return train
+    train["Date"] = pd.to_datetime(train["Date"])
+    train["year"] = train["Date"].dt.year
+    train["month"] = train["Date"].dt.month
+    train["date"] = train["Date"].dt.day
+    train["day"] = train["Date"].dt.dayofweek
+    return train
+
 
 def normalize(train):
-  train = train.drop(["Date", "OpenInt"], axis=1)
-  train_norm = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
-  print(train_norm)
-  return train_norm
+    train = train.drop(["Date"], axis=1) # Convert Date column to datetime
+    # Change all column headings to be lower case, and remove spacing
+    train.columns = [str(x).lower().replace(' ', '_') for x in train.columns]
+    train_norm = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
+    print(train_norm)
+    return train_norm
+
 
 def buildTrain(train, pastDay=30, futureDay=5):
-  X_train, Y_train = [], []
-  for i in range(train.shape[0]-futureDay-pastDay):
-    X_train.append(np.array(train.iloc[i:i+pastDay]))
-    Y_train.append(np.array(train.iloc[i+pastDay:i+pastDay+futureDay]["Close"]))
-  return np.array(X_train), np.array(Y_train)
+    X_train, Y_train = [], []
+    for i in range(train.shape[0] - futureDay - pastDay):
+        X_train.append(np.array(train.iloc[i:i + pastDay]))
+        Y_train.append(np.array(train.iloc[i + pastDay:i + pastDay + futureDay]["adj_close"]))
+    return np.array(X_train), np.array(Y_train)
 
-def shuffle(X,Y):
-  np.random.seed(10)
-  randomList = np.arange(X.shape[0])
-  np.random.shuffle(randomList)
-  return X[randomList], Y[randomList]
 
-def splitData(X,Y,rate):
-  X_train = X[int(X.shape[0]*rate):]
-  Y_train = Y[int(Y.shape[0]*rate):]
-  X_val = X[:int(X.shape[0]*rate)]
-  Y_val = Y[:int(Y.shape[0]*rate)]
-  return X_train, Y_train, X_val, Y_val
+def shuffle(X, Y):
+    np.random.seed(10)
+    randomList = np.arange(X.shape[0])
+    np.random.shuffle(randomList)
+    return X[randomList], Y[randomList]
+
+
+def splitData(X, Y, rate):
+    # cv = df[num_train:num_train + num_cv]
+    # Get sizes of each of the datasets
+    X_train = X[int(X.shape[0] * rate):]
+    Y_train = Y[int(Y.shape[0] * rate):]
+    X_val = X[:int(X.shape[0] * rate)]
+    Y_val = Y[:int(Y.shape[0] * rate)]
+    return X_train, Y_train, X_val, Y_val
+
 
 def buildOneToOneModel(shape):
-  model = Sequential()
-  model.add(LSTM(10, input_length=shape[1], input_dim=shape[2], return_sequences=True))
-  # output shape: (1, 1)
-  model.add(TimeDistributed(Dense(1)))    # or use model.add(Dense(1))
-  model.compile(loss="mse", optimizer="adam")
-  model.summary()
-  return model
+    model = Sequential()
+    model.add(LSTM(10, input_length=shape[1], input_dim=shape[2], return_sequences=True))
+    # output shape: (1, 1)
+    model.add(TimeDistributed(Dense(1)))  # or use model.add(Dense(1))
+    model.compile(loss="mse", optimizer="adam")
+    model.summary()
+    return model
 
-# train = readTrain()
-# train_Aug = augFeatures(train)
-# train_norm = normalize(train_Aug)
-# # change the last day and next day
-# X_train, Y_train = buildTrain(train_norm, 1, 1)
-# X_train, Y_train = shuffle(X_train, Y_train)
-# X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
-#
-# # from 2 dimmension to 3 dimension
-# Y_train = Y_train[:,np.newaxis]
-# Y_val = Y_val[:,np.newaxis]
-#
-# model = buildOneToOneModel(X_train.shape)
-# callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
-# model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
 
 def buildManyToOneModel(shape):
-  model = Sequential()
-  model.add(LSTM(10, input_length=shape[1], input_dim=shape[2]))
-  # output shape: (1, 1)
-  model.add(Dense(1))
-  model.compile(loss="mse", optimizer="adam")
-  model.summary()
-  return model
+    model = Sequential()
+    model.add(LSTM(10, input_length=shape[1], input_dim=shape[2]))
+    # output shape: (1, 1)
+    model.add(Dense(1))
+    model.compile(loss="mse", optimizer="adam")
+    model.summary()
+    return model
 
-start_time=time.time()
+
+start_time = time.time()
 
 train = readTrain()
 train_Aug = augFeatures(train)
 train_norm = normalize(train_Aug)
 # change the last day and next day
-X_train, Y_train = buildTrain(train_norm, 30, 1)
-# X_train, Y_train = shuffle(X_train, Y_train)
+X_train, Y_train = buildTrain(train_norm, 30, 5)
 # because no return sequence, Y_train and Y_val shape must be 2 dimension
 X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
 
@@ -102,16 +97,14 @@ callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
 model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
 # model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val))
 
-end_time=time.time()
-print(end_time-start_time)
+end_time = time.time()
+print(end_time - start_time)
 
 Y_val = Y_val.flatten()
 Y_train = Y_train.flatten()
 
-
 trainPredict = model.predict(X_train).flatten()
 testPredict = model.predict(X_val).flatten()
-
 
 train_size = trainPredict.size
 train_x = [i for i in range(train_size)]
@@ -120,23 +113,10 @@ print(train_x)
 test_size = testPredict.size
 test_x = [i for i in range(test_size)]
 
-
 plt.figure(1)
-# plt.plot(train_x, Y_train, label="trainData")
-# plt.plot(train_x, trainPredict, label="testData")
-plt.plot(test_x,Y_val, label="trainData")
-plt.plot(test_x,testPredict, label="testData")
-plt.xlabel("index")#横坐标名字
-plt.ylabel("Closing price")#纵坐标名字
-plt.legend(loc = "best")#图例
+plt.plot(test_x, Y_val, label="trainData")
+plt.plot(test_x, testPredict, label="testData")
+plt.xlabel("index")  # row axios
+plt.ylabel("Closing price")  # column axios
+plt.legend(loc="best")  # sample
 plt.show()
-
-
-
-
-
-
-
-
-
-
