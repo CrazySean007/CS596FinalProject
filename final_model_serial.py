796668c5f3328654328eac65bbe9d451de3cbe54
CrazySean007 <shuaim@usc.edu>
2020-12-09 15:00:55 -0800

    Add serial and parallel file

--- final_model_serial.py
+++ final_model_serial.py
@@ -1,38 +1,39 @@
 # coding: utf-8
 
+import math
+from datetime import date, datetime, time, timedelta
+from pylab import rcParams
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import time
+from sklearn.preprocessing import PolynomialFeatures
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LassoCV
-from mpi4py import MPI
 
-
-
+start_time = time.time()
 # Input params #
 stk_path = "./data/GSPC.csv"
-# stk_path = "./data/SPY.csv"
-# stk_path = "./data/GSPC.csv"
+
 # train : cv : test = 6 : 2 : 2
 test_size = 0.1  # proportion of dataset to be used as test set
-cv_size = 0.05  # proportion of dataset to be used as cross-validation set
-Nmax = 7  # for feature at day t, we use lags from t-1, t-2, ..., t-N as features
-N = 30
-# Nmax is the maximum N we are going to test
+cv_size = 0.1  # proportion of dataset to be used as cross-validation set
+N = 30  # past days
 
-# parameters needed in MPI
-comm = MPI.COMM_WORLD
-rank, size = comm.Get_rank(), comm.Get_size()
+
+np.random.seed(10)
 
 
 def load_data():
     # # Load data
     df = pd.read_csv(stk_path, sep=",")
-    # df.drop(['Close'], axis = 1)
+
     # Convert Date column to datetime
     df.loc[:, 'Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
     df_date = df['Date']
@@ -61,21 +62,13 @@ def load_data():
     num_cv = int(cv_size * len(df))        # train for meta-regressor
     num_test = int(test_size * len(df))
     num_train = len(df) - num_cv - num_test
-    print("num_train = " + str(num_train))
-    print("num_cv = " + str(num_cv))
-    print("num_test = " + str(num_test))
 
     # Split into train, cv, and test
     train = df[:num_train]
     cv = df[num_train:num_train + num_cv]
     train_cv = df[:num_train + num_cv]
     test = df[num_train + num_cv:]
-    print("train.shape = " + str(train.shape))
-    print("cv.shape = " + str(cv.shape))
-    print("train_cv.shape = " + str(train_cv.shape))
-    print("test.shape = " + str(test.shape))
-
-    return df, train, cv, train_cv, test, num_cv, num_test, num_train
+    return df, train, cv, train_cv, test, num_cv, num_test, num_train,
 
 
 def buildTrain(train, pastDay=30, futureDay=1):
@@ -87,7 +80,6 @@ def buildTrain(train, pastDay=30, futureDay=1):
 
 
 def shuffle(X, Y):
-    np.random.seed(10)
     randomList = np.arange(X.shape[0])
     np.random.shuffle(randomList)
     return X[randomList], Y[randomList]
@@ -101,6 +93,11 @@ def get_mape(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
+def get_smape(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return 2.0 * np.mean(np.abs(y_pred - y_true) / (np.abs(y_pred) + np.abs(y_true))) * 100
+
+
 def lin_regression_model():
 
     def dim3_to_dim2(data):
@@ -109,17 +106,15 @@ def lin_regression_model():
 
     df, train, cv, train_cv, test, num_cv, num_test, num_train = load_data()
 
-    # # Predict using Linear Regression
-    RMSE = []
-    R2 = []
-    mape = []
-
     train = train.drop(["date"], axis=1)
     cv = cv.drop(["date"], axis=1)
     test = test.drop(["date"], axis=1)
-    X_train, y_train = buildTrain(train, 30, 1)
-    X_val, y_val = buildTrain(cv, 30, 1)
-    X_test, y_test = buildTrain(test, 30, 1)
+    X_train, y_train = buildTrain(train, N, 1)
+    X_train, y_train = shuffle(X_train, y_train)
+    X_val, y_val = buildTrain(cv, N, 1)
+    X_val, y_val = shuffle(X_val, y_val)
+    X_test, y_test = buildTrain(test, N, 1)
+    X_test, y_test = shuffle(X_test, y_test)
 
     model = LinearRegression(fit_intercept=True)
     X_train = dim3_to_dim2(X_train)
@@ -127,15 +122,11 @@ def lin_regression_model():
     X_test = dim3_to_dim2(X_test)
     model.fit(X_train, y_train)
 
-    trainPredict = model.predict(X_train).flatten()
     valPredict = model.predict(X_val).flatten()
     testPredict = model.predict(X_test).flatten()
 
-    train_size = trainPredict.size
-    train_x = [i for i in range(train_size)]
     test_size = valPredict.size
     test_x = [i for i in range(test_size)]
-
 
     y_val = y_val.flatten()
     y_train = y_train.flatten()
@@ -174,14 +165,17 @@ def lstm_model():
     cv = cv.drop(["date"], axis=1)
     test = test.drop(["date"], axis=1)
     # change the last day and next day
-    X_train, y_train = buildTrain(train, 30, 1)
-    X_val, y_val = buildTrain(cv, 30, 1)
-    X_test, y_test = buildTrain(test, 30, 1)
+    X_train, y_train = buildTrain(train, N, 1)
+    X_train, y_train = shuffle(X_train, y_train)
+    X_val, y_val = buildTrain(cv, N, 1)
+    X_val, y_val = shuffle(X_val, y_val)
+    X_test, y_test = buildTrain(test, N, 1)
+    X_test, y_test = shuffle(X_test, y_test)
 
     model = buildManyToOneModel(X_train.shape)
-    # callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
-    # model.fit(X_train, y_train, epochs=1000, batch_size=128, validation_data=(X_val, y_val), callbacks=[callback])
-    model.fit(X_train, y_train, epochs=171, batch_size=128, validation_data=(X_val, y_val))
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(X_train, y_train, epochs=1000, batch_size=128, validation_data=(X_val, y_val), callbacks=[callback])
+    # model.fit(X_train, y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val))
 
     y_val = y_val.flatten()
     y_train = y_train.flatten()
@@ -197,7 +191,7 @@ def lstm_model():
 
     plt.figure(1)
     plt.plot(test_x, y_val, label="y_val")
-    plt.plot(test_x, valPredict, label="lstm_predict")
+    plt.plot(test_x, valPredict, label="predict")
     plt.xlabel("index")  # row axios
     plt.ylabel("Closing price")  # column axios
     plt.legend(loc="best")  # sample
@@ -205,9 +199,10 @@ def lstm_model():
 
     valPredict = pd.DataFrame(valPredict, columns=['est_lstm'])
     testPredict = pd.DataFrame(testPredict, columns=['est_lstm'])
+    y_val = pd.DataFrame(y_val, columns=['adj_close'])
+    y_test = pd.DataFrame(y_test, columns=['adj_close'])
 
-    return model, valPredict, testPredict
-
+    return model, valPredict, testPredict, y_val, y_test
 
 '''
     Meta-Regressor: merge two model together
@@ -249,7 +244,6 @@ def ensemble_model(model1_val,
     def get_model_predict(df1, df2, y_data):
         y = y_data[y_target_col]
         X = pd.concat([df1[model1_target_col], df2[model2_target_col]], axis=1)
-        print(X)
         return X, y
 
     models_X_val, y_val = get_model_predict(model1_val, model2_val, y_val)
@@ -259,102 +253,58 @@ def ensemble_model(model1_val,
     models_X_test, y_test = get_model_predict(model1_test, model2_test, y_test)
     models_X_test, y_test = prepare_Xy(models_X_test, y_test)
     testPredict = predict(ensemble_models, models_X_test)
+    testPredict = pd.DataFrame(testPredict, columns=['est_ensem'])
 
     return ensemble_models, testPredict
 
-if rank == 0:
 
-    # load data from the file and start a timer
-    start_time = time.time()
-    data = []
-    print("\n*********************************************\n                    Recv\n*********************************************\n")
+lin_reg_model, lin_reg_val_predict, lin_reg_test_predict, y_val, y_test = lin_regression_model()
 
-    # size = comm.recv(source=1, tag=1)
-    # lin_reg_val_predict = np.empty(size, dtype='float64')
-    lin_reg_val_predict = comm.recv(source=1, tag=2)
-    lin_reg_val_predict = pd.DataFrame(lin_reg_val_predict, columns = ["est_lin_reg"])
-    # print(
-    #     "\n*********************************************\n                    Recv\n*********************************************\n")
-    # print("lin_reg mark1 received")
-    lin_reg_test_predict = comm.recv(source=1, tag=3)
-    lin_reg_test_predict = pd.DataFrame(lin_reg_test_predict, columns = ["est_lin_reg"])
-    # print(
-    #     "\n*********************************************\n                    Recv\n*********************************************\n")
-    # print("lin_reg mark2 received")
-    y_val = comm.recv(source=1, tag=4)
-    y_val = pd.DataFrame(y_val, columns = ["adj_close"])
-    # print(
-    #     "\n*********************************************\n                    Recv\n*********************************************\n")
-    # print("lin_reg mark3 received")
-    y_test = comm.recv(source=1, tag=5)
-    y_test = pd.DataFrame(y_test, columns = ["adj_close"])
-    # print(
-    #     "\n*********************************************\n                    Recv\n*********************************************\n")
-    # print("lin_reg mark4 received")
-    lstm_val_predict = comm.recv(source=2, tag=6)
-    lstm_val_predict = pd.DataFrame(lstm_val_predict, columns = ["est_lstm"])
-    # print(
-    #     "\n*********************************************\n                    Recv\n*********************************************\n")
-    # print("lstm mark1 received")
-    lsmt_test_predict = comm.recv(source=2, tag=7)
-    lsmt_test_predict = pd.DataFrame(lsmt_test_predict, columns = ["est_lstm"])
-    # print(
-    #     "\n*********************************************\n                    Recv\n*********************************************\n")
-    # print("lstm mark12 received")
-    ensemble_models, testPredict = ensemble_model(lin_reg_val_predict, lin_reg_test_predict, lstm_val_predict,
-                                                  lsmt_test_predict, y_val, y_test, 'est_lin_reg', 'est_lstm',
-                                                  'adj_close')
+lstm_model, lstm_val_predict, lstm_test_predict, y_val, y_test = lstm_model()
 
-    test_size = testPredict.size
-    test_x = [i for i in range(test_size)]
-    plt.figure(1)
-    plt.plot(test_x, y_test, label="y_test")
-    plt.plot(test_x, lin_reg_test_predict, label="lin_predict")
-    plt.plot(test_x, lsmt_test_predict, label="lstm_predict")
-    plt.plot(test_x, testPredict, label="ensem_predict")
-    plt.xlabel("index")  # row axios
-    plt.ylabel("Closing price")  # column axios
-    plt.legend(loc="best")  # sample
-    plt.show()
+ensemble_models, ensem_test_predict = ensemble_model(lin_reg_val_predict, lin_reg_test_predict, lstm_val_predict,
+                                              lstm_test_predict, y_val, y_test, 'est_lin_reg', 'est_lstm', 'adj_close')
+
+# #
+RMSE = []
+R2 = []
+MAPE = []
+SMAPE = []
+
+for model_predict in (lin_reg_test_predict, lstm_test_predict, ensem_test_predict):
+    RMSE.append(math.sqrt(mean_squared_error(y_test, model_predict)))
+    R2.append(r2_score(y_test, model_predict))
+    MAPE.append(get_mape(y_test, model_predict))
+    SMAPE.append(get_smape(y_test, model_predict))
+
+print(lin_reg_test_predict)
+
+print(lstm_test_predict)
+
+print(ensem_test_predict)
 
 
-elif rank == 1:
-    # time.sleep(50)
-    lin_reg_model, lin_reg_val_predict, lin_reg_test_predict, y_val, y_test = lin_regression_model()
+# RMSE. Note for RMSE smaller better.
+# R2. Note for R2 larger better.
+# MAPE. Note for MAPE smaller better.
+# SMAPE. Note for SMAPE smaller better.
+print("RMSE" + str(RMSE))
+print("R2" + str(R2))
+print("MAPE" + str(MAPE))
+print("SMAPE" + str(SMAPE))
 
-    lin_reg_val_predict = lin_reg_val_predict.values.flatten()
-    lin_reg_val_predict = np.array(lin_reg_val_predict)
-    comm.send(lin_reg_val_predict, dest=0, tag=2)
-    print("lin_reg mark1")
-
-    lin_reg_test_predict = lin_reg_test_predict.values.flatten()
-    lin_reg_test_predict = np.array(lin_reg_test_predict)
-    comm.send(lin_reg_test_predict, dest=0, tag=3)
-    print("lin_reg mark2")
-
-    y_val = y_val.values.flatten()
-    y_val = np.array(y_val)
-    comm.send(y_val, dest=0, tag=4)
-    print("lin_reg mark3")
-
-    y_test = y_test.values.flatten()
-    y_test = np.array(y_test)
-    comm.send(y_test, dest=0, tag=5)
-    print("lin_reg mark4")
+test_size = ensem_test_predict.size
+test_x = [i for i in range(test_size)]
+plt.figure(10)
+plt.plot(test_x, y_test, label="y_test")
+plt.plot(test_x, lin_reg_test_predict, label="lin_predict")
+plt.plot(test_x, lstm_test_predict, label="lstm_predict")
+plt.plot(test_x, ensem_test_predict, label="ensem_predict")
+plt.xlabel("index")  # row axios
+plt.ylabel("Closing price")  # column axios
+plt.legend(loc="best")  # sample
+end_time = time.time()
+print("Running time: ", start_time-end_time)
+plt.show()
 
 
-elif rank == 2:
-    lstm_model, lstm_val_predict, lsmt_test_predict = lstm_model()
-    # print("*********************************************\nLSTM model*********************************************\n",
-    #       lstm_val_predict)
-    # print("\n*********************************************\nLSTM model*********************************************\n")
-
-    lstm_val_predict = lstm_val_predict.values.flatten()
-    lstm_val_predict = np.array(lstm_val_predict)
-    comm.send(lstm_val_predict, dest=0, tag=6)
-    print("lstm mark1")
-
-    lsmt_test_predict = lsmt_test_predict.values.flatten()
-    lsmt_test_predict = np.array(lsmt_test_predict)
-    comm.send(lsmt_test_predict, dest=0, tag=7)
-    print("lstm mark2")
